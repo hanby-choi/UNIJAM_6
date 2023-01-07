18bb765ae526f0aa821347fba1eb5767ef144ed5
fish_sik <myungsj0308@kaist.ac.kr>
2023-01-07 14:45:23 +0900

    add destroy obstacle & heart system
    destroy obstacle : trigger 주위의 일정 영역에 player가 도달했을 때, space 키를 누르면 어떤 효과가 일어나는 부분까지 구현했습니다.
    
    heart system : 초기에 Hp를 3개로 만들어놨고, obstacle에 충돌하면 1개 감소, car에 충돌하면 3개 감소, Hp가 0이 되면 어떤 효과가 발생하는 부분까지 구현했습니다.
    충돌했을 때, delayTime 동안 무적이 되는 부분 구현했습니다.

---
+++ protectIt/Assets/Scripts/DestroyObstacle.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObstacle : MonoBehaviour
+{
+    bool isCatchable;       //Trigger와의 충돌 여부 확인하는 변수
+    void Start()
+    {
+        isCatchable = false;
+    }
+
+    void Update()
+    {
+        if(isCatchable == true){                            //Trigger와 충돌한 상태이면
+            if(Input.GetKey(KeyCode.Space) == true){    //이 상태에서 space bar를 누르면
+                //파괴 ~~~
+            }
+        }
+    }
+
+    //충돌해 있는 동안 true, 충돌해 있지 않으면 false
+    void OnTriggerEnter2D(Collider2D other){        //Trigger와 충돌하면 isCatchable을 true로 설정
+        if(other.tag == "Trigger")
+            isCatchable = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other){         //Trigger로 부터 분리되면 isCatchable을 false로 설정
+        if(other.tag == "Trigger")
+            isCatchable = false;
+    }
+}
