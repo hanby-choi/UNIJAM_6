9d6234e6b8bcd2091db2a395a123d67d29c08ea8
fish_sik <myungsj0308@kaist.ac.kr>
2023-01-07 17:48:25 +0900

    add Item & modify playermovement
    item : 캐맄터가 아이템을 먹었을 때, 효과 구현했습니다.
    playermovement : 코드 정리했습니다.

---
+++ protectIt/Assets/Scripts/Item.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public float increaseRate;      //이속 증가율
+    public float itemDelayTime;     //아이템 지속시간(B타입에만 해당)
+    private bool isEffective;       //아이템 효과가 나타나는지 여부
+    private bool isCrashed;         //아이템이 닿았는지 여부
+
+    void Start()
+    {
+        isEffective = false;
+        isCrashed = false;
+    }
+
+    void Update()
+    {
+        if(isCrashed == true){                      //아이템과 충돌하면
+            isEffective = true;                     //아이템 효과 발생 처리
+            StartCoroutine(OnTriggerEnter2D());     //coroutine 시작
+            isCrashed = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(isEffective == false){
+            if(other.tag == "Item_A"){
+                other.gameObject.SetActive(false);
+                GameObject.Find("Character").GetComponent<HeartSystem>().Hp++;
+            }
+
+            if(other.tag == "Item_B"){
+                other.gameObject.SetActive(false);  //닿은 아이템 비활성화
+                GameObject.Find("Character").GetComponent<PlayerMovement>().playerRigidbody.velocity = GameObject.Find("Character").GetComponent<PlayerMovement>().playerRigidbody.velocity * increaseRate;  //이동속도 증가(효과)
+                isCrashed = true;                   //충돌 처리
+            }
+        }
+    }
+
+    IEnumerator OnTriggerEnter2D(){
+        yield return new WaitForSeconds(itemDelayTime); //일정 시간 대기
+        isEffective = false;                            //아이템 효과 없어짐 처리
+        GameObject.Find("Character").GetComponent<PlayerMovement>().playerRigidbody.velocity = GameObject.Find("Character").GetComponent<PlayerMovement>().playerRigidbody.velocity / increaseRate;           //이동속도 복원
+    }
+}
+
+// Item과 충돌하면 비활성화
+// 각자의 효과를 얻게됨.
+
+//B 타입 아이템 -> 이속 몇 초간 30% 증가
+//GameObject.Find("Character").GetComponent<PlayerMovement>().playerRigidbody.velocity
--- protectIt/Assets/Scripts/PlayerMovement.cs
+++ protectIt/Assets/Scripts/PlayerMovement.cs
@@ -4,28 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D playerRigidbody;  //  이동에 사용할 rigidbody component
-    Vector2 playerVelocity; //  player의 속도벡터 선언
+    public Rigidbody2D playerRigidbody;  //  이동에 사용할 rigidbody component
+    private SpriteRenderer playerSRenderer; //Xflip에 사용할 객체
+    private Vector2 playerVelocity; //  player의 속도벡터
 
-    public float playerSpeed;
+    public float playerSpeed;       // 플레이어 이동 속도
     public float changeRateMin;    //  최소 방향전환 주기
     public float changeRateMax;   //  최대 방향전환 주기
-    private float changeRate;    //  방향전환 주기
-    private float timeAfterChange;   //  최근 방향전환 시점에서 지난 시간
+    private float changeRate;    //  방향전환 주기(난수)
+    private float timeAfterChange;   //  마지막 방향전환 시점에서 지난 시간(측정 시간)
 
-    private SpriteRenderer playerSRenderer;  
 
     void Start()
     {
-        playerVelocity = new Vector2(playerSpeed, 0);    //  player의 초기속도 선언
         playerRigidbody = GetComponent<Rigidbody2D>();    //  게임 오브젝트에서 2D Rigidbody component 찾아서 playerRigidbody에 할당
-        
+        playerSRenderer = GetComponent<SpriteRenderer>(); //  게임 오브젝트에서  component 찾아서 playerSRenderer에 할당
+
+        playerVelocity = new Vector2(playerSpeed, 0);    //  player의 초기속도 선언
         playerRigidbody.velocity = playerVelocity;  //  player의 속도 설정
 
         timeAfterChange = 0f;
         changeRate = Random.Range(changeRateMin, changeRateMax);    //방향전환 주기 값 설정(Random)
 
-        playerSRenderer = GetComponent<SpriteRenderer>(); //  게임 오브젝트에서  component 찾아서 playerSRenderer에 할당
+        
     }
 
     
@@ -34,21 +35,16 @@ public class PlayerMovement : MonoBehaviour
         timeAfterChange += Time.deltaTime;      //  마지막 방향 전환 이후 시간 측정
 
         /*playerVelocity = Vector2(playerSpeed * transform.forward);
-        playerRigidbody.velocity = playerVelocity;  */
+        playerRigidbody.velocity = playerVelocity;  */      //추후 수정 예정
 
 
-        if(timeAfterChange >= changeRate)   //  방향전환 주기보다 클 경우
+        if(timeAfterChange >= changeRate)   //  측정시간이 방향전환 주기보다 클 경우
         {
-            timeAfterChange = 0f;
-            
-            playerVelocity = -playerVelocity;   //방향 전환
-            playerRigidbody.velocity = playerVelocity;  //player의 속도 설정
+            playerRigidbody.velocity = -playerRigidbody.velocity;  //player의 이동 방향 전환
+            playerSRenderer.flipX = !playerSRenderer.flipX;         //player flipX
 
+            timeAfterChange = 0f;           //  측정시간 0으로 초기화
             changeRate = Random.Range(changeRateMin, changeRateMax);    //방향전환 주기 값 재설정
-
-            if(playerVelocity.x > 0) playerSRenderer.flipX = false;    //방향 전환 후 이동방향이 +면 flipX를 false로 설정
-            else playerSRenderer.flipX = true;   //방향 전환 후 이동방향이 -면 true로 설정
-
         }
     }
 }
